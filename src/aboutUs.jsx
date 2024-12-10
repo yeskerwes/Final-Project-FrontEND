e747a3f894d42a67ad7e9a3bc6c04cdb7f8c6dc7
Ayazhan Turarkhanova <aya.t@Ayazhans-MacBook-Pro.local>
2024-12-10 16:47:05 +0500

    new section About Us

--- src/aboutUs.jsx
+++ src/aboutUs.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import './styles/aboutUs.css';
+import aboutUsPicture from './images/aboutUs-picture.png';
 
 const AboutUs = () => {
     return (
         <div className="aboutUs-container">
-            <p>Welcome to Kicks Avenue, your ultimate destination for premium footwear <br/> and streetwear. Founded with a passion for sneakers and fashion, <br />
-             we aim to provide a curated selection of the latest and most iconic styles, <br /> blending comfort and innovation for every step you take.</p>
-
+            <div className="aboutUs-photo">
+                <img src={aboutUsPicture} alt="About Us" />
+            </div>
+            <div className="aboutUs-content">
+                <h1>About Us</h1>
+                <p>
+                    Welcome to Kicks Avenue, your ultimate destination for premium footwear 
+                    and streetwear. Founded with a passion for sneakers and fashion, 
+                    we aim to provide a curated selection of the latest and most iconic styles, 
+                    blending comfort and innovation for every step you take.
+                </p>
+                <button className="aboutUs-button">Learn More</button>
+            </div>
         </div>
     );
 };
