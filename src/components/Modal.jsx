b96d657663cea74c2a7d65c2938090dba0538952
Samratik <samrat.kakimgali05@gmail.com>
2024-12-19 14:19:31 +0500

    debugging dubplicate css files

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,9 +3,9 @@ import "../styles/modal.css";
 
 const Modal = ({ content, onClose }) => {
   return (
-    <div className="modal-overlay" onClick={onClose}>
-      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <button className="modal-close" onClick={onClose}>
+    <div className="modal-overlay-icons" onClick={onClose}>
+      <div className="modal-content-icons" onClick={(e) => e.stopPropagation()}>
+        <button className="modal-close-icons" onClick={onClose}>
           &times;
         </button>
         <div>{content}</div>
