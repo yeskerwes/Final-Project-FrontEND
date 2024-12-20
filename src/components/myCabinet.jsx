9f0323640c5b7069c3fcfd0cd3b8b66703d6342b
Samratik <samrat.kakimgali05@gmail.com>
2024-12-21 01:00:05 +0500

    nearly finished my cabinet

--- src/components/myCabinet.jsx
+++ src/components/myCabinet.jsx
@@ -5,7 +5,6 @@ import { ReactComponent as Profile } from '../images/icon-profile.svg';
 import { ReactComponent as TriangleDown } from '../images/arrow-down-triangle.svg';
 import { ReactComponent as Logout } from '../images/logout.svg';
 
-
 function MyCabinet() {
   const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
@@ -18,19 +17,23 @@ function MyCabinet() {
     setIsOpen(false);
   };
 
-  const handleClick = () => {
-    navigate('/profile'); // Перенаправление на страницу профиля
+  const handleNavigate = (path) => {
+    navigate(path); // Перенаправление на указанный путь
   };
+  
 
   return (
     <div className="profile-container">
-        <div className="profile-trigger"
-            onClick={handleClick}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}>
-            <Profile className="icon-myProfile" /> My Profile
-            <TriangleDown className='icon-triangleDown'/>
-          </div>
+      <div
+        className="profile-trigger"
+        onClick={() => handleNavigate('/profile')} // Перенаправление на страницу профиля
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <Profile className="icon-myProfile" /> My Profile
+        <TriangleDown className="icon-triangleDown" />
+      </div>
+
       {isOpen && (
         <div
           className="profile-panel"
@@ -38,16 +41,15 @@ function MyCabinet() {
           onMouseLeave={handleMouseLeave}
         >
           <ul>
-            <li>Current orders</li>
-            <li>Personal Account</li>
-            <li>Personal Details</li>
-            <li>Change password</li>
-            <li>Order history</li>
-            <li>Basket</li>
-            <li>Favorites</li>
-            <li>Contacts</li>
-            <li className="logout"><Logout 
-            className='icon-logout'/>Log Out</li>
+            <li onClick={() => handleNavigate('/current-orders')}>Current orders</li>
+            <li onClick={() => handleNavigate('/personal-account')}>Personal Account</li>
+            <li onClick={() => handleNavigate('/person-details')}>Personal Details</li>
+            <li onClick={() => handleNavigate('/change-password')}>Change password</li>
+            <li onClick={() => handleNavigate('/basket')}>Basket</li>
+            <li onClick={() => handleNavigate('/contacts')}>Contacts</li>
+            <li className="logout" onClick={() => handleNavigate('/logout')}>
+              <Logout className="icon-logout" /> Log Out
+            </li>
           </ul>
         </div>
       )}
